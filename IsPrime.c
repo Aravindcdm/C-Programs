b4f74908f678134420754dd1986838d9018c4ad5
ARAVIND CHIDAMBARAM R <149189593+Aravindcdm@users.noreply.github.com>
2024-08-15 22:14:35 +0530

    Update IsPrime.c

--- IsPrime.c
+++ IsPrime.c
@@ -1,17 +1,16 @@
 // Online C compiler to run C program online
 #include <stdio.h>
 int prime(int n){
-    int flag=1;
     if(n==0 || n==1)
-    return 1;
+    return 0;
     else
     {
        for(int i=2;i<n;i++){
            if(n%i==0)
-           flag = 0;
+           return 0;
        } 
     }
-    return flag;
+    return 1;
 }
 int main() {
     int num;
